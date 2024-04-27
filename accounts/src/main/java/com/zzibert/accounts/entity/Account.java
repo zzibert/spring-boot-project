54bb5687f20f63a6701c01e85b8a495bf9822f49
Žan Žibert <zzibert@plume.com>
2024-04-27 11:14:15 +0200

    Writing Spring Data JPA entities & repositories to interact with DB tables

---
+++ accounts/src/main/java/com/zzibert/accounts/entity/Account.java
@@ -0,0 +1,31 @@
+package com.zzibert.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account extends BaseEntity {
+
+    @Column(name="customer_id")
+    private Long customerId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="account_number")
+    private Long accountNumber;
+
+    @Column(name="account_type")
+    private String accountType;
+
+    private String name;
+
+    private String email;
+
+    @Column(name="branch_address")
+    private String branchAddress;
+}
---
+++ accounts/src/main/java/com/zzibert/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.zzibert.accounts.repository;
+
+import com.zzibert.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
