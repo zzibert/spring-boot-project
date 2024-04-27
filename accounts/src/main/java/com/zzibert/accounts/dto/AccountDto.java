3d48df338ee8c3ad6fa0aa34a65845714b9d4ce0
Žan Žibert <zzibert@plume.com>
2024-04-27 11:39:47 +0200

    Creating DTOs inside accounts microservice

---
+++ accounts/src/main/java/com/zzibert/accounts/dto/AccountDto.java
@@ -0,0 +1,13 @@
+package com.zzibert.accounts.dto;
+
+
+import lombok.Data;
+
+@Data
+public class AccountDto {
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
