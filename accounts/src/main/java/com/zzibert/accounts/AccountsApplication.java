e73a6c185ab0266bb3b2bbcef8b9655e7d8382b6
Žan Žibert <zzibert@plume.com>
2024-05-03 07:43:09 +0200

    Generate Docker Image of Accounts microservice with Dockerfile - Part 1

--- accounts/src/main/java/com/zzibert/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/zzibert/accounts/AccountsApplication.java
@@ -1,11 +1,20 @@
 package com.zzibert.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "AuditAwareImpl")
+@OpenAPIDefinition(
+  info = @Info(
+    title = "Accounts microservice REST API Documentation",
+    description = "EazyBank Accounts microservice REST API Documentation",
+    version = "v1"
+  )
+)
 public class AccountsApplication {
 
   public static void main(String[] args) {
